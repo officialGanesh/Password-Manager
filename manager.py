3c1feef9afed292f934a10a11f1ac2cf6a5204d1
Ganesh <ganuofficial2795@gmail.com>
2021-07-08 19:28:02 +0530

    Passcode generated

--- manager.py
+++ manager.py
@@ -2,10 +2,28 @@ import fire, string, random, pymongo, hashlib
 import pyinputplus as pyip
 
 
-def main():
+def main(length):
     """ Main Script Function """
 
+    str_passcode = Generate_password(length)
+    
 
+def Generate_password(password_length):
+    """ Generating the password """
+
+    letters = string.ascii_letters
+    digits = string.digits
+    extras = string.punctuation
+
+    values = []
+    values.extend(letters)
+    values.extend(digits)
+    values.extend(extras)
+
+    secret_code = "".join(random.sample(values,password_length))
+
+    print(secret_code)
+    return secret_code
 
 
 
@@ -13,4 +31,4 @@ if __name__ == "__main__":
 
     fire.Fire(main)
     print("Code Completed 🚀")
-    
+
