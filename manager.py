3914c44bd76fe3b9f5bb5916baae38aad243aefc
Ganesh <ganuofficial2795@gmail.com>
2021-07-08 19:13:33 +0530

    Initial Commit

---
+++ manager.py
@@ -0,0 +1,16 @@
+import fire, string, random, pymongo, hashlib
+import pyinputplus as pyip
+
+
+def main():
+    """ Main Script Function """
+
+
+
+
+
+if __name__ == "__main__":
+
+    fire.Fire(main)
+    print("Code Completed 🚀")
+    
